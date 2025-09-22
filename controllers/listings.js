68b9e3a1731c02bf6afc3cb26831cbb0d650cb67
Vivek329 <viveksingh.jsr27@gmail.com>
2025-09-22 19:06:17 +0530

    showing image while in edit

--- controllers/listings.js
+++ controllers/listings.js
@@ -42,7 +42,9 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_100,w_100")
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
 }
 
 module.exports.updateListing = async (req, res) => {
